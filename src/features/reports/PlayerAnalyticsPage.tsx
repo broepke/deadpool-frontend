29059f5721bdccda7d751f9789726f687b9f8c57
Brian Roepke <broepke@gmail.com>
2025-02-24 14:37:53 -0500

    Removed unused stuff

--- src/features/reports/PlayerAnalyticsPage.tsx
+++ src/features/reports/PlayerAnalyticsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { getPlayerAnalytics } from '../../api/services/reporting';
 import type { PlayerAnalyticsResponse } from '../../api/services/reporting';
 import { LoadingSpinner } from '../../components/common/LoadingSpinner';
